3991cee2307d4475c1520adcef22357e2584339f
John Kilima <jkbundala@gmail.com>
2022-12-02 16:13:16 +0300

    Cleaning up base project

--- resources/views/hello.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout')
-@section('content')
-    <h3>New from hello view</h3>
-    {{ $variable }} <br>
-    {{ $blog -> title }} <br>
-    @foreach ( $array as $arr )
-        {{ $arr }}
-    @endforeach
-@stop
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('hello', 'App\Http\Controllers\BlogController@create');
 Route::get('dashboard', 'App\Http\Controllers\DashboardController@index');
